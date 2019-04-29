a898d6348a8f101468e80a9fd523f306ad7e4cd0
cache-sk <cache.sk@gmail.com>
2019-04-29 18:14:31 +0200

    version 1.1.3

--- repository/regenerate.py
+++ repository/regenerate.py
@@ -1,26 +1,26 @@
 import os
 
 def delete_all_files(folder,skip):
-	print('processing '+folder)
-	for the_file in os.listdir(folder):
-		file_path = os.path.join(folder, the_file)
-		#print(file_path)
-		if not file_path in skip:
-			try:
-				if os.path.isfile(file_path):
-					print('unlinking '+file_path)
-					os.unlink(file_path)
-				elif os.path.isdir(file_path):
-					delete_all_files(file_path,skip)
-					print('removing '+file_path)
-					os.rmdir(file_path)
-			except Exception as e:
-				print(e)
-		else:
-			print('skipping '+file_path)
+    print('processing '+folder)
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        #print(file_path)
+        if not file_path in skip:
+            try:
+                if os.path.isfile(file_path):
+                    print('unlinking '+file_path)
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    delete_all_files(file_path,skip)
+                    print('removing '+file_path)
+                    os.rmdir(file_path)
+            except Exception as e:
+                print(e)
+        else:
+            print('skipping '+file_path)
 
 SKIP = [os.path.join('.', 'create_repository.py'),
-		os.path.join('.', 'regenerate.py')]
+        os.path.join('.', 'regenerate.py')]
 
 delete_all_files('.',SKIP)
-os.system("create_repository.py ../plugin.video.yabop/")
+os.system("python create_repository.py ../plugin.video.yabop/")
